273377996665b5729f08e1d67097914e0a749624
robin <rokim8@gmail.com>
2024-01-26 02:00:40 -0500

    Farcaster frames support (#4)
    * fresh yarn install, gitignore
    
    * handle farcaster POST requests
    
    * add integration tests
    
    * bump node
    
    * node bump
    
    * retry

---
+++ src/pages/api/opengraph/image.test.ts
@@ -0,0 +1,102 @@
+import axios, { AxiosResponse } from "axios";
+
+// TODO: add this to github CI; will need to ensure the server is running beforehand
+describe("/pages/api/opengraph/image", () => {
+  describe("clicking Next on first token in carousel", () => {
+    let response: AxiosResponse;
+    let html: string;
+    beforeAll(async () => {
+      response = await axios.post(
+        "http://localhost:3001/api/opengraph/image?path=/opengraph/user/curated",
+        {
+          untrustedData: { buttonIndex: 1 },
+        }
+      );
+
+      html = response.data;
+    });
+
+    it("should return 200", () => {
+      expect(response.status).toBe(200);
+      expect(response.headers["content-type"]).toBe("text/html");
+    });
+
+    it("should provide Next and Previous button options", () => {
+      expect(html).toContain('<meta property="fc:frame" content="vNext">');
+      expect(html).toContain('<meta property="fc:frame:button:1" content="←">');
+      expect(html).toContain('<meta property="fc:frame:button:2" content="→">');
+    });
+
+    it("should increment the position parameter", () => {
+      expect(html).toContain(
+        '<meta property="fc:frame:image" content="https://gallery.so/opengraph/user/curated?position=1">'
+      );
+    });
+  });
+
+  describe("clicking Next on second token", () => {
+    let response: AxiosResponse;
+    let html: string;
+    beforeAll(async () => {
+      response = await axios.post(
+        "http://localhost:3001/api/opengraph/image?path=/opengraph/user/curated&position=1",
+        {
+          untrustedData: { buttonIndex: 2 },
+        }
+      );
+
+      html = response.data;
+    });
+
+    it("should return 200", () => {
+      expect(response.status).toBe(200);
+      expect(response.headers["content-type"]).toBe("text/html");
+    });
+
+    it("should provide Next and Previous button options", () => {
+      expect(html).toContain('<meta property="fc:frame" content="vNext">');
+      expect(html).toContain('<meta property="fc:frame:button:1" content="←">');
+      expect(html).toContain('<meta property="fc:frame:button:2" content="→">');
+    });
+
+    it("should increment the position parameter", () => {
+      expect(html).toContain(
+        '<meta property="fc:frame:image" content="https://gallery.so/opengraph/user/curated?position=2">'
+      );
+    });
+  });
+
+  describe("clicking Previous on second token", () => {
+    let response: AxiosResponse;
+    let html: string;
+    beforeAll(async () => {
+      response = await axios.post(
+        "http://localhost:3001/api/opengraph/image?path=/opengraph/user/curated&position=1",
+        {
+          untrustedData: { buttonIndex: 1 },
+        }
+      );
+
+      html = response.data;
+    });
+
+    it("should return 200", () => {
+      expect(response.status).toBe(200);
+      expect(response.headers["content-type"]).toBe("text/html");
+    });
+
+    it("should only provide Next button, and not a Previous button", () => {
+      expect(html).toContain('<meta property="fc:frame" content="vNext">');
+      expect(html).not.toContain(
+        '<meta property="fc:frame:button:1" content="←">'
+      );
+      expect(html).toContain('<meta property="fc:frame:button:1" content="→">');
+    });
+
+    it("should not include a position parameter", () => {
+      expect(html).toContain(
+        '<meta property="fc:frame:image" content="https://gallery.so/opengraph/user/curated">'
+      );
+    });
+  });
+});

1daa358990d6d337de7289f1682c0da918ad2bd9
Rohan-cp <rohancp9@icloud.com>
2024-01-28 15:20:55 -0500

    added post query in og route

--- src/pages/api/og.tsx
+++ src/pages/api/og.tsx
@@ -1,7 +1,7 @@
-import { NextRequest, NextResponse } from 'next/server';
 import { ImageResponse } from '@vercel/og';
 import { fetchWithJustQueryText } from '../fetch';
 import { postIdQuery } from '../queries/postIdOpengraphQuery';
+import { NextRequest } from 'next/server';
 
 
 let baseUrl = "https://gallery.so";
@@ -20,9 +20,96 @@ export const config = {
   runtime: 'edge',
 };
 
-const handler = async (request: NextRequest, res: NextResponse) => {
-  const { searchParams } = new URL(request.url);
-/*
+export default async function handler(request: NextRequest){
+  try {
+    const { searchParams } = request.nextUrl;
+    const postId = searchParams.get('postId');
+    
+    const queryResponse = await fetchWithJustQueryText({
+      queryText: postIdQuery,
+      variables: { postId: postId },
+    });
+  
+    if (!postId || !queryResponse?.data?.post) {
+      return new ImageResponse((
+         <>Visit gallery.so</>
+      ), {
+        width: 1200,
+        height: 630,
+      });
+    }
+    
+    const post = queryResponse.data.post;
+    const author = post.author;
+    
+    return new ImageResponse( <div style={{
+      display: 'flex',
+      lineHeight: '32px',
+    }}
+  >
+    <p
+      style={{
+        fontFamily: "'ABCDiatype-Regular'",
+        fontSize: '25px',
+        fontWeight: 400,
+        lineHeight: '32px',
+        display: '-webkit-box',
+        WebkitBoxOrient: 'vertical',
+        WebkitLineClamp: 5,
+        wordBreak: 'break-all',
+        maxWidth: '350px',
+        margin: 0,
+      }}
+    >
+      {post?.caption}
+    </p>
+  </div>
+  );
+    
+    return new ImageResponse(
+      (
+        <div
+          style={{
+            fontSize: 40,
+            display: 'flex',
+            color: 'black',
+            background: 'white',
+            width: '100%',
+            height: '100%',
+            padding: '50px 200px',
+            textAlign: 'center',
+            justifyContent: 'center',
+            alignItems: 'center',
+          }}
+        >
+          👋 Hello 你好 नमस्ते こんにちは สวัสดีค่ะ 안녕 добрий день Hallá {postId}
+        </div>
+      ),
+      {
+        width: 1200,
+        height: 630,
+      },
+    );
+  } catch(e){
+    return null;
+  }
+  
+  /*
+  const path = request.query.path as string;
+  
+  if (!path) {
+    res.status(400).json({ error: { code: "MISSING_PATH" } });
+    return;
+  }
+
+  const url = new URL(path, baseUrl);
+
+  if (!url.toString().startsWith(baseUrl)) {
+    res.status(400).json({ error: { code: "INVALID_PATH" } });
+    return;
+  }
+  
+
    if (!path) {
      res.status(400).json({ error: { code: "MISSING_PATH" } });
      return;
@@ -50,7 +137,7 @@ const handler = async (request: NextRequest, res: NextResponse) => {
 //   const height = parseInt(req.query.height as string) || 630;
 //   const pixelDensity = parseInt(req.query.pixelDensity as string) || 2;
  
-   const postId = searchParams.get('postId') ?? '';
+   const postId = '2awUGpHrVTyUCgZfAXZAMJSSpl7';
 
    try {
       const queryResponse = await fetchWithJustQueryText({
@@ -113,4 +200,3 @@ const handler = async (request: NextRequest, res: NextResponse) => {
    }
  };
  
- export default handler;

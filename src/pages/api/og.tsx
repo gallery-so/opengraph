b9291e7098bd061d820021a5cba737684dcaef56
Rohan-cp <rohancp9@icloud.com>
2024-01-28 15:36:06 -0500

    post og image working

--- src/pages/api/og.tsx
+++ src/pages/api/og.tsx
@@ -7,6 +7,10 @@ import { NextRequest } from 'next/server';
 let baseUrl = "https://gallery.so";
 let apiBaseUrl = "https://gallery-opengraph.vercel.app";
 
+export const WIDTH_OPENGRAPH_IMAGE = 1200;
+export const HEIGHT_OPENGRAPH_IMAGE = 628;
+
+
 // can manually set the preview URL via environment variables on vercel for the `opengraph` service
 if (process.env.NEXT_PUBLIC_PREVIEW_URL) {
   baseUrl = process.env.NEXT_PUBLIC_PREVIEW_URL;
@@ -16,6 +20,26 @@ if (process.env.NEXT_PUBLIC_PREVIEW_URL) {
   apiBaseUrl = "https://gallery-opengraph-preview.vercel.app";
 }
 
+type UrlSet = {
+  small: string | null;
+  medium: string | null;
+  large: string | null;
+};
+
+const ABCDiatypeRegular = fetch(
+  new URL('../assets/fonts/ABCDiatype-Regular.ttf', import.meta.url)
+).then((res) => res.arrayBuffer());
+
+const ABCDiatypeBold = fetch(
+  new URL('../assets/fonts/ABCDiatype-Bold.ttf', import.meta.url)
+).then((res) => res.arrayBuffer());
+
+const alpinaLight = fetch(
+  new URL('../assets/fonts/GT-Alpina-Standard-Light.ttf', import.meta.url)
+).then((res) => res.arrayBuffer());
+
+
+
 export const config = {
   runtime: 'edge',
 };
@@ -24,6 +48,9 @@ export default async function handler(request: NextRequest){
   try {
     const { searchParams } = request.nextUrl;
     const postId = searchParams.get('postId');
+    let postImageUrl =
+    'https://assets.gallery.so/https%3A%2F%2Fstorage.googleapis.com%2Fprod-token-content%2F4-292cd-KT1EfsNuqwLAWDd3o4pvfUx1CAh5GMdTrRvr-image?auto=compress%2Cformat&fit=max&glryts=1705844681&w=1024&s=9076d07060aeb7ac31297a0381bd0ed3';
+
     
     const queryResponse = await fetchWithJustQueryText({
       queryText: postIdQuery,
@@ -41,122 +68,177 @@ export default async function handler(request: NextRequest){
     
     const post = queryResponse.data.post;
     const author = post.author;
+    const firstLetter = author?.username?.substring(0, 1).toUpperCase() ?? '';
+
+    let profileImageUrl = null;
+    const { token: profileToken, profileImage } = post?.author?.profileImage ?? {};
+  
+    if (profileImage && profileImage.previewURLs?.medium) {
+      profileImageUrl = profileImage.previewURLs.medium;
+    }
+  
+    const media = profileToken?.definition?.media;
+  
+    let previewUrls: UrlSet | null = null;
+  
+    if (!profileToken) {
+      return null;
+    }
     
-    return new ImageResponse( <div style={{
-      display: 'flex',
-      lineHeight: '32px',
-    }}
-  >
-    <p
+    if (
+      media &&
+      'previewURLs' in media &&
+      media.previewURLs &&
+      (media.previewURLs.small || media.previewURLs.medium || media.previewURLs.large)
+    ) {
+      previewUrls = media.previewURLs;
+    } else if ('fallbackMedia' in media) {
+      if (media.fallbackMedia?.mediaURL) {
+        previewUrls = {
+          small: media.fallbackMedia.mediaURL,
+          medium: media.fallbackMedia.mediaURL,
+          large: media.fallbackMedia.mediaURL,
+        };
+      }
+    }
+  
+    profileImageUrl = previewUrls?.small;
+  
+    const postToken = post.tokens?.[0];
+    if (!postToken) {
+      return null;
+    }
+  
+    const postMedia = postToken?.definition?.media;
+    let postPreviewUrls: UrlSet | null = null;
+  
+    if (
+      postMedia &&
+      'previewURLs' in postMedia &&
+      postMedia.previewURLs &&
+      (postMedia.previewURLs.small || postMedia.previewURLs.medium || postMedia.previewURLs.large)
+    ) {
+      postPreviewUrls = postMedia.previewURLs;
+    } else if ('fallbackMedia' in media) {
+      if (media.fallbackMedia?.mediaURL) {
+        postPreviewUrls = {
+          small: media.fallbackMedia.mediaURL,
+          medium: media.fallbackMedia.mediaURL,
+          large: media.fallbackMedia.mediaURL,
+        };
+      }
+    }
+  
+    postImageUrl = postPreviewUrls?.large ?? '';  
+    
+    const ABCDiatypeRegularFontData = await ABCDiatypeRegular;
+    const ABCDiatypeBoldFontData = await ABCDiatypeBold;
+    const alpinaLightFontData = await alpinaLight;  
+  
+    return new ImageResponse((
+      <div
       style={{
-        fontFamily: "'ABCDiatype-Regular'",
-        fontSize: '25px',
-        fontWeight: 400,
-        lineHeight: '32px',
-        display: '-webkit-box',
-        WebkitBoxOrient: 'vertical',
-        WebkitLineClamp: 5,
-        wordBreak: 'break-all',
-        maxWidth: '350px',
-        margin: 0,
+        height: '100%',
+        width: '100%',
+        display: 'flex',
+        gap: '70px',
+        alignItems: 'center',
+        justifyContent: 'center',
+        backgroundColor: '#fff',
       }}
     >
-      {post?.caption}
-    </p>
-  </div>
-  );
-    
-    return new ImageResponse(
-      (
+      <svg
+        style={{ width: '56.74px', height: '196px' }}
+        width="40"
+        height="121"
+        viewBox="0 0 36 121"
+        fill="none"
+        xmlns="http://www.w3.org/2000/svg"
+      >
+        <path
+          d="M33.3845 120.5C-4.79681 97.5124 -11.3149 43.5986 19.4718 11.6054C22.5802 8.39033 25.9865 5.47922 29.6447 2.91146C30.5382 2.3054 32.4485 1.05912 33.3845 0.500008L35.0311 3.09922C16.1957 15.7113 4.47411 37.8411 4.63154 60.5C4.47411 83.159 16.1957 105.314 35.0311 117.901L33.3845 120.5Z"
+          fill="#141414"
+        />
+      </svg>
+      <div
+        style={{
+          display: 'flex',
+          gap: '70px',
+          alignItems: 'center',
+          justifyContent: 'center',
+        }}
+      >
+        <img
+          width="370"
+          src={postImageUrl}
+          style={{
+            maxWidth: '370px',
+            maxHeight: '370px',
+            display: 'block',
+          }}
+          alt="post"
+        />
         <div
           style={{
-            fontSize: 40,
             display: 'flex',
-            color: 'black',
-            background: 'white',
-            width: '100%',
-            height: '100%',
-            padding: '50px 200px',
-            textAlign: 'center',
-            justifyContent: 'center',
-            alignItems: 'center',
+            flexDirection: 'column',
+            gap: '8px',
           }}
         >
-          👋 Hello 你好 नमस्ते こんにちは สวัสดีค่ะ 안녕 добрий день Hallá {postId}
-        </div>
-      ),
-      {
-        width: 1200,
-        height: 630,
-      },
-    );
-  } catch(e){
-    return null;
-  }
-  
-  /*
-  const path = request.query.path as string;
-  
-  if (!path) {
-    res.status(400).json({ error: { code: "MISSING_PATH" } });
-    return;
-  }
+          <div
+            style={{
+              display: 'flex',
+              alignItems: 'center',
+              gap: '8px',
+            }}
+          >
+            {profileImageUrl ? (
+              <img
+                width="48"
+                height="48"
+                src={profileImageUrl}
+                style={{
+                  borderRadius: 30,
+                }}
+                alt="profile picture"
+              />
+            ) : (
+              <div
+                style={{
+                  height: 48,
+                  width: 48,
+                  fontSize: 28,
+                  borderWidth: 1,
+                  borderColor: 'black',
+                  fontFamily: "'GT Alpina'",
 
-  const url = new URL(path, baseUrl);
-
-  if (!url.toString().startsWith(baseUrl)) {
-    res.status(400).json({ error: { code: "INVALID_PATH" } });
-    return;
-  }
-  
-
-   if (!path) {
-     res.status(400).json({ error: { code: "MISSING_PATH" } });
-     return;
-   }
-   
-   
-   
-
- 
-   const url = new URL(path, baseUrl);
- 
-   if (!url.toString().startsWith(baseUrl)) {
-     res.status(400).json({ error: { code: "INVALID_PATH" } });
-     return;
-   }
- 
-
- 
-   const fallback =
-     typeof req.query.fallback === "string" ? req.query.fallback : null;
-        */
- 
-   // default should mirror https://github.com/gallery-so/gallery/blob/main/src/constants/opengraph.ts
-//  const width = parseInt(req.query.width as string) || 1200;
-//   const height = parseInt(req.query.height as string) || 630;
-//   const pixelDensity = parseInt(req.query.pixelDensity as string) || 2;
- 
-   const postId = '2awUGpHrVTyUCgZfAXZAMJSSpl7';
-
-   try {
-      const queryResponse = await fetchWithJustQueryText({
-         queryText: postIdQuery,
-         variables: { postId: postId },
-       });
-     
-       if (!postId || !queryResponse?.data?.post) {
-         return new ImageResponse((
-            <>Visit gallery.so</>
-         ), {
-           width: 1200,
-           height: 630,
-         });
-       }
-     
-       const post = queryResponse.data.post;
-       const author = post.author;
-       return ( <div style={{
+                  display: 'flex',
+                  justifyContent: 'center',
+                  alignItems: 'center',
+                  borderRadius: '999999999px',
+                }}
+              >
+                {firstLetter}
+              </div>
+            )}
+            <h1
+              style={{
+                fontSize: '32px',
+                lineHeight: '36px',
+                fontFamily: "'ABCDiatype-Bold'",
+                letterSpacing: '-0.01em',
+                margin: '0',
+                paddingBottom: 4,
+                display: 'flex',
+                alignItems: 'center',
+                justifyContent: 'center',
+              }}
+            >
+              {author?.username}
+            </h1>
+            
+          </div>
+          <div style={{
                 display: 'flex',
                 lineHeight: '32px',
               }}
@@ -178,25 +260,48 @@ export default async function handler(request: NextRequest){
                 {post?.caption}
               </p>
             </div>
-            );
-
-     
-   } catch (error: unknown) {
-     // TODO: log this to some error tracking service?
-     console.error("error while generating opengraph image", error);
- /*
-     if (fallback) {
-       res.redirect(fallback);
-       return;
-     }
- */
-
-     res.status(500).json({
-       error: {
-         code: "UNEXPECTED_ERROR",
-         message: (error as any).toString(),
-       },
-     });
-   }
+        </div>
+      </div>
+      <svg
+        style={{ width: '56.74px', height: '196px' }}
+        width="20"
+        height="194"
+        viewBox="0 0 36 121"
+        fill="none"
+        xmlns="http://www.w3.org/2000/svg"
+      >
+        <path
+          d="M2.61129 0.500008C40.7972 23.4876 47.3153 77.4014 16.5284 109.395C13.4189 112.609 10.0126 115.52 6.35534 118.089C5.4576 118.695 3.55158 119.941 2.61555 120.5L0.968933 117.901C19.8045 105.289 31.5261 83.159 31.3687 60.5C31.5261 37.8411 19.8045 15.7113 0.968933 3.09922L2.61129 0.500008Z"
+          fill="#141414"
+        />
+      </svg>
+    </div>
+    ),
+    {
+          width: WIDTH_OPENGRAPH_IMAGE,
+          height: HEIGHT_OPENGRAPH_IMAGE,
+          fonts: [
+            {
+              name: 'ABCDiatype-Regular',
+              data: ABCDiatypeRegularFontData,
+              weight: 400,
+            },
+            {
+              name: 'ABCDiatype-Bold',
+              data: ABCDiatypeBoldFontData,
+              weight: 700,
+            },
+            {
+              name: 'GT Alpina',
+              data: alpinaLightFontData,
+              style: 'normal',
+              weight: 500,
+            },
+          ],
+        }
+    )
+  } catch(e){
+    return null;
+  }
  };
  

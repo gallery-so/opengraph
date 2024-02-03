43039eb6840e8add914b9b83c74b0f96cc13e7eb
Robinnnnn <rokim8@gmail.com>
2024-02-03 18:55:57 -0500

    add additional media type support for posts, collections

--- src/queries/collectionIdOpengraphQuery.ts
+++ src/queries/collectionIdOpengraphQuery.ts
@@ -16,29 +16,144 @@ export const collectionIdIdOpengraphQuery = `query CollectionIdOpengraphQuery($c
                     dbid
                     definition {
                         media {
+                            ... on AudioMedia {
+                              __typename
+                              previewURLs {
+                                small
+                                medium
+                                large
+                              }
+                              fallbackMedia {
+                                mediaURL
+                              }
+                            }
+                            ... on GltfMedia {
+                              __typename
+                              previewURLs {
+                                small
+                                medium
+                                large
+                              }
+                              fallbackMedia {
+                                mediaURL
+                              }
+                            }
+                            ... on HtmlMedia {
+                              __typename
+                              previewURLs {
+                                small
+                                medium
+                                large
+                              }
+                              fallbackMedia {
+                                mediaURL
+                              }
+                            }
                             ... on ImageMedia {
-                                __typename
-                                previewURLs {
-                                    small
-                                    medium
-                                    large
-                                }
-                                fallbackMedia {
-                                    mediaURL
-                                }
+                              __typename
+                              previewURLs {
+                                small
+                                medium
+                                large
+                              }
+                              fallbackMedia {
+                                mediaURL
+                              }
+                            }
+                            ... on GIFMedia {
+                              __typename
+                              staticPreviewURLs {
+                                small
+                                medium
+                                large
+                              }
+                              previewURLs {
+                                small
+                                medium
+                                large
+                              }
+                              fallbackMedia {
+                                mediaURL
+                              }
+                            }
+                            ... on JsonMedia {
+                              __typename
+                              previewURLs {
+                                small
+                                medium
+                                large
+                              }
+                              fallbackMedia {
+                                mediaURL
+                              }
+                            }
+                            ... on TextMedia {
+                              __typename
+                              previewURLs {
+                                small
+                                medium
+                                large
+                              }
+                              fallbackMedia {
+                                mediaURL
+                              }
+                            }
+                            ... on PdfMedia {
+                              __typename
+                              previewURLs {
+                                small
+                                medium
+                                large
+                              }
+                              fallbackMedia {
+                                mediaURL
+                              }
+                            }
+                            ... on UnknownMedia {
+                              __typename
+                              previewURLs {
+                                small
+                                medium
+                                large
+                              }
+                              fallbackMedia {
+                                mediaURL
+                              }
+                            }
+                            ... on InvalidMedia {
+                              __typename
+                              previewURLs {
+                                small
+                                medium
+                                large
+                              }
+                              fallbackMedia {
+                                mediaURL
+                              }
+                            }
+                            ... on SyncingMedia {
+                              __typename
+                              previewURLs {
+                                small
+                                medium
+                                large
+                              }
+                              fallbackMedia {
+                                mediaURL
+                              }
                             }
                             ... on VideoMedia {
-                                __typename
-                                previewURLs {
-                                    small
-                                    medium
-                                    large
-                                }
-                                fallbackMedia {
-                                    mediaURL
-                                }
+                              __typename
+                              previewURLs {
+                                small
+                                medium
+                                large
+                              }
+                              fallbackMedia {
+                                mediaURL
+                              }
                             }
-                        }
+                          }
                     }
                 }
             }
--- src/queries/postIdOpengraphQuery.ts
+++ src/queries/postIdOpengraphQuery.ts
@@ -171,7 +171,122 @@ export const postIdQuery = `
           dbid
           definition {
             media {
+              ... on AudioMedia {
+                __typename
+                previewURLs {
+                  small
+                  medium
+                  large
+                }
+                fallbackMedia {
+                  mediaURL
+                }
+              }
+              ... on GltfMedia {
+                __typename
+                previewURLs {
+                  small
+                  medium
+                  large
+                }
+                fallbackMedia {
+                  mediaURL
+                }
+              }
+              ... on HtmlMedia {
+                __typename
+                previewURLs {
+                  small
+                  medium
+                  large
+                }
+                fallbackMedia {
+                  mediaURL
+                }
+              }
               ... on ImageMedia {
+                __typename
+                previewURLs {
+                  small
+                  medium
+                  large
+                }
+                fallbackMedia {
+                  mediaURL
+                }
+              }
+              ... on GIFMedia {
+                __typename
+                staticPreviewURLs {
+                  small
+                  medium
+                  large
+                }
+                previewURLs {
+                  small
+                  medium
+                  large
+                }
+                fallbackMedia {
+                  mediaURL
+                }
+              }
+              ... on JsonMedia {
+                __typename
+                previewURLs {
+                  small
+                  medium
+                  large
+                }
+                fallbackMedia {
+                  mediaURL
+                }
+              }
+              ... on TextMedia {
+                __typename
+                previewURLs {
+                  small
+                  medium
+                  large
+                }
+                fallbackMedia {
+                  mediaURL
+                }
+              }
+              ... on PdfMedia {
+                __typename
+                previewURLs {
+                  small
+                  medium
+                  large
+                }
+                fallbackMedia {
+                  mediaURL
+                }
+              }
+              ... on UnknownMedia {
+                __typename
+                previewURLs {
+                  small
+                  medium
+                  large
+                }
+                fallbackMedia {
+                  mediaURL
+                }
+              }
+              ... on InvalidMedia {
+                __typename
+                previewURLs {
+                  small
+                  medium
+                  large
+                }
+                fallbackMedia {
+                  mediaURL
+                }
+              }
+              ... on SyncingMedia {
                 __typename
                 previewURLs {
                   small

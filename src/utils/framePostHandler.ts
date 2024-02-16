135a5ee6987529c7cb552d2f5b3e46765826bc60
Rohan-cp <rohancp9@icloud.com>
2024-02-16 17:30:35 -0500

    test

--- src/utils/framePostHandler.ts
+++ src/utils/framePostHandler.ts
@@ -29,6 +29,25 @@ export async function framePostHandler(req: NextApiRequest, isExplore?: boolean)
     if (Number(position) === 1 && Number(buttonIndex) === 1) {
       hasPrevious = isExplore ?? false;
       url.searchParams.delete('position');
+
+      const headers = new Headers();
+      headers.append('Content-Type', 'text/html');
+
+      return new Response(
+        `
+      <html>
+        <meta property="fc:frame" content="vNext">
+        <meta property="fc:frame:button:1" content="EXPLORE">
+        <meta property="fc:frame:image" content="${url}">
+        <meta property="fc:frame:post_url" content="${url}">
+        <body>gm</body>
+      </html>
+    `,
+        {
+          status: 200,
+          headers,
+        },
+      );
     } else if (Number(buttonIndex) === 1) {
       // `prev` should decrement the position
       url.searchParams.set('position', `${Number(position) - 1}`);
@@ -44,24 +63,6 @@ export async function framePostHandler(req: NextApiRequest, isExplore?: boolean)
 
   const headers = new Headers();
   headers.append('Content-Type', 'text/html');
-
-  if (isExplore) {
-    return new Response(
-      `
-      <html>
-        <meta property="fc:frame" content="vNext">
-        <meta property="fc:frame:button:2" content="EXPLORE">
-        <meta property="fc:frame:image" content="${url}">
-        <meta property="fc:frame:post_url" content="${url}">
-        <body>gm</body>
-      </html>
-    `,
-      {
-        status: 200,
-        headers,
-      },
-    );
-  }
 
   return new Response(
     `
